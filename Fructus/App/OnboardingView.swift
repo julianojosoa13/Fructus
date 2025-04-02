8251445cf4590d32b315c8f3afdc8061d86e7103
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 14:04:39 +0300

    Adds horizontal padding to the FruitCardView

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - PREVIEW
+#Preview {
+    OnboardingView()
+}
