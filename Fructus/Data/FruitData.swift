5934edc28578b28868e87851bc8a405fcb00ac77
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 15:05:37 +0300

    First fruit data: Blueberries

---
+++ Fructus/Data/FruitData.swift
@@ -0,0 +1,14 @@
+//
+//  FruitData.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+// MARK: - Fruits Data
+
+let fruitsData: [Fruit] = [
+    Fruit(title: "Blueberry", headline: "Blueberries are sweat, nutritious and wildly popular fruit all over the world.", image: "blueberry", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "Blueberries are perenial flowering plants with blue or purple berries", nutrition: ["240 KJ","9.96 g","0.33 g","0.74 g", "A, B1", "Calcium, Magnesium"])
+]
