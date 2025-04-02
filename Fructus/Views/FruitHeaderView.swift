28ad1f93002a3381031d87a8134d4dbe4c252c06
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 20:57:21 +0300

    FruitHeader component initial layout

--- Fructus/Views/FruitHeaderView.swift
+++ Fructus/Views/FruitHeaderView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct FruitHeaderView: View {
+    var fruit: Fruit
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: .black.opacity(0.15), radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
+        } //: ZSTACK
+        .frame(height: 440)
     }
 }
 
 #Preview {
-    FruitHeaderView()
+    FruitHeaderView(fruit: fruitsData[0])
+        .previewLayout(.fixed(width: 370, height: 440))
 }
