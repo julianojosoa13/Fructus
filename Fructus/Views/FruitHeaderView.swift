c3575009573b230899b840fe7200ed239b8d63a3
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 21:06:48 +0300

    FruitHeader component Image scale animation

--- Fructus/Views/FruitHeaderView.swift
+++ Fructus/Views/FruitHeaderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FruitHeaderView: View {
     var fruit: Fruit
+    @State private var isAnimatingImage: Bool = false
     
     var body: some View {
         ZStack {
@@ -19,12 +20,18 @@ struct FruitHeaderView: View {
                 .scaledToFit()
                 .shadow(color: .black.opacity(0.15), radius: 8, x: 6, y: 8)
                 .padding(.vertical, 20)
+                .scaleEffect(isAnimatingImage ? 1.0:0.6)
         } //: ZSTACK
         .frame(height: 440)
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimatingImage = true
+            }
+        })
     }
 }
 
 #Preview {
-    FruitHeaderView(fruit: fruitsData[0])
+    FruitHeaderView(fruit: fruitsData[1])
         .previewLayout(.fixed(width: 370, height: 440))
 }
