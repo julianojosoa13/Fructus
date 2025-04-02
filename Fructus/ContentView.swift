deffde4ea55709dba4cf8ab40e3f39c8a0b2c24d
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 10:50:34 +0300

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
