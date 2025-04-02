e0ef72bf0a56a19da500f0bd9329b41319169734
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 20:43:59 +0300

    Fruit Header component

---
+++ Fructus/Views/FruitHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitHeaderView()
+}
