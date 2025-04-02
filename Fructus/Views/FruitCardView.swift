9c06f3459dc2d99737b3ae222062a074c2e3aa09
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 11:48:07 +0300

    Initial project structure

---
+++ Fructus/Views/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}
