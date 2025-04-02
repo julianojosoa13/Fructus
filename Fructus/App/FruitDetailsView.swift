4bf881d24653fe27536ffad5d9d66ba1803ecc48
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 19:55:28 +0300

    FruitDetailsView

---
+++ Fructus/App/FruitDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitDetailsView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct FruitDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitDetailsView()
+}
