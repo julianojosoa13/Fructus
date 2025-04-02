5169d0855ca6f7dbeab2f301d803369b9c4ca018
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 20:39:28 +0300

    FruitDetailsView initial Layout

--- Fructus/App/FruitDetailsView.swift
+++ Fructus/App/FruitDetailsView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 struct FruitDetailsView: View {
     var fruit: Fruit
     var body: some View {
-        Text(fruit.title)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // HEADER
+                    
+                    VStack(alignment: .leading,spacing: 20) {
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // NUTRIENTS
+                        
+                        
+                        // SUBHEADLINES
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .font(.subheadline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        // LINK
+                        
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                } //: VStack
+            } //: SCROLL
+        } //: NAVIGATION
     }
 }
 
