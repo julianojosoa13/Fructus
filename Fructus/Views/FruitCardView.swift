071f331dade737e3b0974eee55a454f12e625686
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 12:38:48 +0300

    FruitCardView main sections

--- Fructus/Views/FruitCardView.swift
+++ Fructus/Views/FruitCardView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct FruitCardView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red:0, green:0, blue:0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red:0,green:0, blue:0,opacity: 0.15), radius: 2, x:2, y:2)
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutrucious and wildly popular fruits all over the world.")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // BUTTON: START
+                
+            } //: VSTACK
+        } //: ZSTACK
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
+// MARK: - PREVIEW
+
 #Preview {
     FruitCardView()
+        .previewLayout(.fixed(width: 320, height: 640))
 }
---
+++ Fructus/Views/StartButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Juliano RAHAJAHARIMANGA on 02/04/2025.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartButtonView()
+}
