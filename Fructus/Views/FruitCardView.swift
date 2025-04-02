dd34f091d324ab772c3ed579b663f9f85240ceb1
Juliano Josoa Rahajaharimanga <josoajuliano@gmail.com>
2025-04-02 13:54:29 +0300

    Finished the FruitCardView design

--- Fructus/Views/FruitCardView.swift
+++ Fructus/Views/FruitCardView.swift
@@ -35,7 +35,7 @@ struct FruitCardView: View {
                     .frame(maxWidth: 480)
                 
                 // BUTTON: START
-                
+                StartButtonView()
             } //: VSTACK
         } //: ZSTACK
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
